9254fbbc9d35b6aa5cb8f0fdd05e284ece2f26be
sickofworkingforaliving <andrew.sicko.jones@gmail.com>
2026-01-18 21:05:44 +0000

    Add function to generate CSV report
    Implemented CSV generation for weekly report.

--- src/generate_report.py
+++ src/generate_report.py
@@ -1,1 +1,8 @@
-#TODO: generate reprt
+import pandas as pd
+import os
+
+def generate_csv(uk_listings, jp_processed):
+    df = pd.DataFrame(jp_processed)
+
+    output_path = os.path.join("data", "jimny-weekly-report.csv")
+    df.to_csv(output_path, index=False)
