ea6011b76af4c1c1d6bbb311bda31f4bde491eca
sickofworkingforaliving <andrew.sicko.jones@gmail.com>
2026-01-18 21:06:53 +0000

    Implement function to get FX rate for GBP

--- src/fx.py
+++ src/fx.py
@@ -1,1 +1,7 @@
-#TODO: exchange rates
+import requests
+
+def get_fx_rate():
+    url = "https://api.exchangerate.host/latest?base=JPY&symbols=GBP"
+    r = requests.get(url)
+    data = r.json()
+    return data["rates"]["GBP"]
