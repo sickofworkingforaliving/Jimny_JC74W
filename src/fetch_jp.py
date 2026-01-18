dfee55d5153f472f3da38b6ba9f6b8b7af8dfed6
sickofworkingforaliving <andrew.sicko.jones@gmail.com>
2026-01-18 21:12:39 +0000

    Implement fetch_jp_auctions function

--- src/fetch_jp.py
+++ src/fetch_jp.py
@@ -1,1 +1,6 @@
-#TODO: Japan scraper
+def fetch_jp_auctions():
+    # Example placeholder data
+    return [
+        {"hammer_jpy": 1800000, "year": 2020, "miles": 12000, "trans": "manual"},
+        {"hammer_jpy": 2000000, "year": 2021, "miles": 8000, "trans": "auto"}
+    ]
